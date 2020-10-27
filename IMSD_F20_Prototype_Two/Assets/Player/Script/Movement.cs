3543d6096583f0da51e5a908cbdda1385e06d8a4
Bryan Santiago <musicprostar11@gmail.com>
2020-10-27 16:12:14 -0400

    Rough prototype Started

---
+++ IMSD_F20_Prototype_Two/Assets/Player/Script/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+	public CharacterController PlayerController;
+
+	Vector3 PlayerMovement;
+	public float PlayerX;
+	public float PlayerZ;
+
+    void Start()
+    {
+        PlayerController = GetComponent<CharacterController>();
+        PlayerMovement = transform.right * PlayerX + transform.forward * PlayerZ;
+    }
+
+    void Update()
+    {
+    	if(Input.GetKey(KeyCode.W))
+    	{
+    		transform.position += PlayerMovement;
+    		Debug.Log("Forward");
+    	}
+    	else if(Input.GetKey(KeyCode.S))
+    	{
+    		transform.position += -PlayerMovement;
+    		Debug.Log("Backwards");
+    	}
+    	else if(Input.GetKey(KeyCode.D))
+    	{
+    		Debug.Log("Right");
+    	}
+    }
+}
