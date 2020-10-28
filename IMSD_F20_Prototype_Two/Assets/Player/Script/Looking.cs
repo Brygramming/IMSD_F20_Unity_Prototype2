59b78139284fd6a9677b40ee05adbdfd5172b239
Bryan Santiago <musicprostar11@gmail.com>
2020-10-28 01:08:17 -0400

    Added Script
    Added movement script for players to move the character.

--- IMSD_F20_Prototype_Two/Assets/Player/Script/Looking.cs
+++ IMSD_F20_Prototype_Two/Assets/Player/Script/Looking.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Looking : MonoBehaviour
 {
-    // Start is called before the first frame update
+	//Multipler
+	public float MouseSensitivity = 200f;
+	//Mouse Positions
+	float MouseX;
+	float MouseY;
+
+	//Player Body
+	public Transform Body;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        //Player Body Turn
+        Body.Rotate(Vector3.up * MouseX);
+        //Camera Rotation
+        transform.Rotate(Vector3.right * -MouseY);
     }
 }
